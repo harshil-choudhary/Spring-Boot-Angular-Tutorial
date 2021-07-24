0fc70bede1dafec785ddd3b34b2f3845de3e0346
Harshil <harshil.choudhary@paytmmall.com>
2021-07-25 00:01:15 +0530

    First commit

---
+++ src/test/java/com/harshil/fullstacktutorial/FullStackTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshil.fullstacktutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
